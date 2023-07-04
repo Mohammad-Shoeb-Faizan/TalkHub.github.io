921a9679a9614154bf76ab1d4d16095a6c27a747
MOHAMMAD SHOEB FAIZAN <shoebfaizan71@gmail.com>
2023-07-05 00:24:25 +0530

    Chat App

---
+++ frontend/src/ChatsPage/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="210d7bce-a967-43c2-89ae-ef6008adcf8f"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
